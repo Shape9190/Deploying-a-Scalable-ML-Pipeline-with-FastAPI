fc14c2ed312f7f68f26899b366c90da2225db50c
Laura Kruize <lkruize@gmail.com>
2024-05-23 18:43:11 -0700

    pickle is trouble

--- test_ml.py
+++ test_ml.py
@@ -14,13 +14,20 @@ from ml.model import (
 from train_model import cat_features
 
 
-project = "Deploying-a-Scalable-ML-Pipeline-with-FastAPI/"
-data_path = os.path.join(project, "data", "census.csv")
-data = pd.read_csv(data_path,delimiter=",")
 
+#project = "Deploying-a-Scalable-ML-Pipeline-with-FastAPI/"
+#data_path = os.path.join(project, "data", "census.csv")
+#data = pd.read_csv(data_path,delimiter=",")
+
+@pytest.fixture
+def data():
+    path = "~/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/"
+    df_path = os.path.join(path,"data/census.csv")
+    df = pd.read_csv(df_path)
+    return df
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_shape():
+def test_shape(data: pd.DataFrame):
     """
     # Ensure data is without null values
     """
@@ -29,7 +36,7 @@ def test_shape():
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_process_data():
+def test_process_data(data: pd.DataFrame):
     """
     # Confirm process_data function performing as intended
     """
@@ -44,11 +51,12 @@ def test_process_data():
     assert y_train.shape[0] > 0
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_pickle():
+def pickle_made(data: pd.DataFrame):
     """
-    # Check that model saved to pickle 
+    Confirm the pickle files were made 
     """
-    pe = os.path.join(project, "model", "encoder.pkl")
-    pm = os.path.join(project, "model", "model.pkl")
-    assert os.path.exists(pe)
-    assert os.path.exists(pm)
+    project = "~/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/"
+    model_pickle = os.path.join(project, "model", "model.pkl")
+    
+    assert os.path.exists(model_pickle)
+
